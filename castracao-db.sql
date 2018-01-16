8efe6720c9e14e95fb95d0bd4321f3bafca059ce
Renato Lelis <renatojlelis@gmail.com>
2018-01-15 22:02:57 -0300

    Add 'castracao' db

---
+++ castracao-db.sql
@@ -0,0 +1,13 @@
+/*Criação da tabelas de castração*/
+create table castracao(
+    cpf varchar(11),
+    nome_dono varchar(255) not null,
+    telefone varchar(9) not null,
+    endereco varchar(255) not null,
+    nome_animal varchar(255) not null,
+    especie_animal ENUM('Cachorro', 'Gato') not null,
+    raca_animal varchar(255) not null,
+    atendido boolean not null,
+    dataAtendimento date not null,
+    primary key(cpf)
+);
